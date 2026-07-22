c3b1bde055a556a774cc24f4d8feb1fca9011442
Dhruv Singh <dhruvsingh8922@gmail.com>
2026-07-22 11:54:17 +0530

    API for get all enrollment of an logged in student with status

--- src/main/java/com/arishi/lms_backend/repo/EnrollmentRepo.java
+++ src/main/java/com/arishi/lms_backend/repo/EnrollmentRepo.java
@@ -1,9 +1,16 @@
 package com.arishi.lms_backend.repo;
 import com.arishi.lms_backend.entity.Enrollment;
 import com.arishi.lms_backend.entity.Student;
+import com.arishi.lms_backend.dto.response.StudentEnrollmentCourseResponseDTO;
 import com.arishi.lms_backend.entity.Course;
 import com.arishi.lms_backend.enums.EnrollmentStatus;
+
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface EnrollmentRepo extends JpaRepository<Enrollment, Long> {
@@ -11,4 +18,36 @@ public interface EnrollmentRepo extends JpaRepository<Enrollment, Long> {
     boolean existsByStudentIdAndCourseIdAndStatusAndDeletedAtIsNull(Long studentId, Long courseId, EnrollmentStatus status);
 
     boolean existsByStudentAndCourseAndDeletedAtIsNull(Student student, Course course);
+    @Query("""
+            SELECT new com.arishi.lms_backend.dto.response.StudentEnrollmentCourseResponseDTO(c.id,c.title,c.description, i.id,
+            CONCAT(i.firstName,' ',i.lastName), c.startDate,c.endDate, c.price, e.createdAt )
+            FROM Enrollment e
+            JOIN e.course c
+            JOIN c.instructor i
+            WHERE e.student.id = :studentId
+            AND e.status = :status
+            AND e.deletedAt IS NULL
+            AND c.deletedAt IS NULL
+            AND c.endDate >= :today
+            ORDER BY e.createdAt DESC""")
+        List<StudentEnrollmentCourseResponseDTO> findOngoingEnrollments(
+                @Param("studentId") Long studentId,@Param("status") EnrollmentStatus status, @Param("today") LocalDate today
+        );
+        @Query("""
+            SELECT new com.arishi.lms_backend.dto.response.StudentEnrollmentCourseResponseDTO( c.id,c.title,c.description, i.id,
+            CONCAT(i.firstName,' ',i.lastName), c.startDate,c.endDate,c.price, e.createdAt )
+            FROM Enrollment e
+            JOIN e.course c
+            JOIN c.instructor i
+            WHERE e.student.id = :studentId
+            AND e.status = :status
+            AND e.deletedAt IS NULL
+            AND c.deletedAt IS NULL
+            AND c.endDate < :today
+            ORDER BY e.createdAt DESC """)
+        List<StudentEnrollmentCourseResponseDTO> findCompletedEnrollments(
+                @Param("studentId") Long studentId,
+                @Param("status") EnrollmentStatus status,
+                @Param("today") LocalDate today
+        );
 }
--- src/main/java/com/arishi/lms_backend/service/impl/EnrollmentServiceImpl.java
+++ src/main/java/com/arishi/lms_backend/service/impl/EnrollmentServiceImpl.java
@@ -2,11 +2,14 @@ package com.arishi.lms_backend.service.impl;
 
 import com.arishi.lms_backend.config.security.CurrentUser;
 import com.arishi.lms_backend.dto.response.EnrollmentResponseDTO;
+import com.arishi.lms_backend.dto.response.StudentEnrollmentCourseResponseDTO;
 import com.arishi.lms_backend.entity.Course;
 import com.arishi.lms_backend.entity.Enrollment;
 import com.arishi.lms_backend.entity.Student;
+import com.arishi.lms_backend.enums.EnrollmentStatus;
 import com.arishi.lms_backend.exception.customException.BadRequestException;
 import com.arishi.lms_backend.exception.customException.DuplicateResourceException;
+import com.arishi.lms_backend.exception.customException.ForbiddenException;
 import com.arishi.lms_backend.exception.customException.ResourceNotFoundException;
 import com.arishi.lms_backend.mapper.EnrollmentMapper;
 import com.arishi.lms_backend.repo.CourseRepo;
@@ -19,6 +22,7 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -26,7 +30,7 @@ public class EnrollmentServiceImpl implements EnrollmentService {
 
     private final StudentRepo studentRepository;
     private final CourseRepo courseRepository;
-    private final EnrollmentRepo enrollmentRepo;
+    private final EnrollmentRepo enrollmentRepository;
 
     @Override
     @Transactional
@@ -45,7 +49,7 @@ public class EnrollmentServiceImpl implements EnrollmentService {
         Course course = courseRepository.findByIdAndDeletedAtIsNull(courseId).orElseThrow(() -> new ResourceNotFoundException("Course not found"));
 
         //duplicate enrollment check
-        if (enrollmentRepo.existsByStudentAndCourseAndDeletedAtIsNull(student, course)) {
+        if (enrollmentRepository.existsByStudentAndCourseAndDeletedAtIsNull(student, course)) {
             throw new DuplicateResourceException("Student is already enrolled in this course");
         }
 
@@ -58,8 +62,40 @@ public class EnrollmentServiceImpl implements EnrollmentService {
 
         Enrollment enrollment = EnrollmentMapper.toEntity(student, course);
 
-        Enrollment savedEnrollment = enrollmentRepo.save(enrollment);
+        Enrollment savedEnrollment = enrollmentRepository.save(enrollment);
 
         return EnrollmentMapper.toEnrollmentResponseDTO(savedEnrollment);
     }
+
+        @Override
+        public List<StudentEnrollmentCourseResponseDTO> getStudentEnrollments(String status) {
+
+            Long studentId = CurrentUser.getId();
+            String role = CurrentUser.getRole();
+
+            if (!"STUDENT".equalsIgnoreCase(role)) {
+                throw new ForbiddenException("Access denied.");}
+
+            LocalDate today = LocalDate.now();
+
+            switch (status.toLowerCase()) {
+
+                case "ongoing":
+                    return enrollmentRepository.findOngoingEnrollments(
+                            studentId,
+                            EnrollmentStatus.APPROVED,
+                            today);
+
+                case "completed":
+                    return enrollmentRepository.findCompletedEnrollments(
+                            studentId,
+                            EnrollmentStatus.APPROVED,
+                            today );
+
+                default:
+                    throw new BadRequestException(
+                            "Invalid status. Allowed values are ongoing and completed." );
+            }
+        }
+    
 }
